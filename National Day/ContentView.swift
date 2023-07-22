e8b56286f9770b8b79835615e0b1fd07bf8f195b
Toh Jia Xuan <jiaxuan.toh10@rafflesgirlssch.edu.sg>
2023-07-22 11:36:20 +0800

    Added tabs

--- National Day/ContentView.swift
+++ National Day/ContentView.swift
@@ -14,8 +14,7 @@ struct ContentView: View {
     @State private var attendedParade = 1
     @State private var songRating = 5.0
     var body: some View {
-       // TabView{
-         //   tabItem(){
+        TabView{
         VStack {
             Text("National Day Is Coming!")
                 .foregroundColor(.red)
@@ -24,32 +23,32 @@ struct ContentView: View {
                 .font(.system(size: flagSize))
             Text("\(flagSize)")
             Slider(value:$flagSize, in: 50...500)
-            //           }
-            //       }
-            //      tabItem(){
-            Form {
-                Section("A button") {
-                    Button("Press me if you like National Day!"){
-                        print("I was pressed")
-                    }
-                }
-                Section ("National Day Songs"){
-                    TextField("Song", text: $textEntered)
-                        .textFieldStyle(.roundedBorder)
-                    Text(textEntered == " " ? " " : "Your favourite song is \(textEntered)")
-                }
-                Section("Other important info") {
-                    Toggle("Do you like national day celebrations?", isOn: $likesCeleb)
-                    Stepper("You have attended National Day Parade for \(attendedParade)", value: $attendedParade)
-                    Text("Your rating for '2023 National Day Song Shine Your Light': \(songRating)")
-                    Slider(value: $songRating, in: 0...10)
-                }
             }
-            
+        .tabItem{
+            Label("Homepage", systemImage: "person")
+            }
+        Form {
+          Section("A button") {
+            Button("Press me if you like National Day!"){
+                        print("I was pressed")
+            }
+          }
+          Section ("National Day Songs"){
+            TextField("Song", text: $textEntered)
+              .textFieldStyle(.roundedBorder)
+            Text(textEntered == " " ? " " : "Your favourite song is \(textEntered)")
+            }
+          Section("Other important info") {
+            Toggle("Do you like national day celebrations?", isOn: $likesCeleb)
+            Stepper("You have attended National Day Parade for \(attendedParade)", value: $attendedParade)
+            Text("Your rating for '2023 National Day Song Shine Your Light': \(songRating)")
+            Slider(value: $songRating, in: 0...10)
+           }
         }
-      //      }
-            
-     //   }//end of tabview
+        .tabItem{
+            Label("Form", systemImage: "newspaper.fill")
+        }
+     }//end of tabview
         
     }
 }
